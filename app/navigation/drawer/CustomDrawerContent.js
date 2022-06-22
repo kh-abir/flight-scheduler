9d8519da63df127db5c25c51141dacd0085c83a9
Kamrul Hassan <kh27.abir@gmail.com>
2022-06-22 20:14:26 +0600

    authentication using async storage and redux

--- app/navigation/drawer/CustomDrawerContent.js
+++ app/navigation/drawer/CustomDrawerContent.js
@@ -1,88 +1,163 @@
-import {connect, useDispatch} from "react-redux";
-import {setSelectedTab} from "../../stores/tab/tabActions";
-import {DrawerContentScrollView} from "@react-navigation/drawer";
-import {View, Alert} from "react-native";
-import {COLORS, SIZES} from "../../constants/theme";
-import CustomDrawerItem from "./CustomDrawerItem";
-import React from "react";
-import {name, version} from '../../../package.json';
-import {DRAWER_ITEMS} from "../../constants/drawerItems";
-import CloseMenu from "../../components/CloseMenu";
+import { connect } from 'react-redux'
+import { DrawerContentScrollView } from '@react-navigation/drawer'
+import { View, Alert } from 'react-native'
+import { COLORS, SIZES } from '../../constants/theme'
+import CustomDrawerItem from './CustomDrawerItem'
+import React, { useState, useEffect } from 'react'
+import { name, version } from '../../../package.json'
+import { DRAWER_ITEMS } from '../../constants/drawerItems'
+import CloseMenu from '../../components/CloseMenu'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { authenticate } from '../../stores/auth/authActions'
+import { setSelectedTab } from '../../stores/tab/tabActions'
+import axios from 'axios'
+import TherapymateLogo from '../../components/TherapymateLogo'
+import { useToast } from 'react-native-toast-notifications'
 
 const CustomDrawerContent = (props) => {
-    const {navigation} = props
-    const dispatch = useDispatch()
-    return (
-        <DrawerContentScrollView
-            scrollEnabled={true}
-            contentContainerStyle={{flex: 1}}
-        >
-            <View style={{flex: 1, paddingHorizontal: SIZES.radius}}>
-                {/*Close Menu*/}
-                <CloseMenu navigation={navigation}/>
-                {/*Profile*/}
-                {/*Drawer Items*/}
-                <View style={{flex: 1, marginTop: SIZES.padding}}>
-                    {DRAWER_ITEMS.slice(0, 7).map(({id, label, icon, screen}) =>
-                        <CustomDrawerItem
-                            key={id}
-                            label={label}
-                            icon={icon}
-                            isFocused={props.selectedTab === label}
-                            onPress={() => {
-                                dispatch(setSelectedTab(label))
-                                navigation.navigate(screen)
-                            }}
-                        />
-                    )}
+  const {
+    navigation,
+    isAuthenticated,
+    setAuthenticated,
+    setSelectedTab,
+    selectedTab,
+  } = props
 
-                    <View
-                        style={{
-                            height: 1,
-                            marginVertical: 40,
-                            marginLeft: SIZES.radius,
-                            backgroundColor: COLORS.darkBlue
-                        }}
-                    />
-
-                    {DRAWER_ITEMS.slice(7, 10).map(({id, label, icon, iconType, screen}) =>
-                        <CustomDrawerItem
-                            key={id}
-                            label={label}
-                            icon={icon}
-                            iconType={iconType}
-                            isFocused={props.selectedTab === label}
-                            onPress={() => {
-                                dispatch(setSelectedTab(label))
-                                screen === 'AppInfoScreen' ?
-                                    Alert.alert('App Info', `Name: ${name}\nVersion: ${version}`, [{text: 'OK'}]) :
-                                    navigation.navigate(screen)
-                            }}
-                        />
-                    )}
-                </View>
-
-                {/*Login Button*/}
-                <View style={{marginBottom: SIZES.padding}}>
-                    <CustomDrawerItem
-                        label={'Login'}
-                        icon={'login'}
-                        isFocused={props.selectedTab === 'Login'}
-                        onPress={() => {
-                            dispatch(setSelectedTab('Login'))
-                            navigation.navigate('LoginScreen')
-                        }}
-                    />
-                </View>
-            </View>
-        </DrawerContentScrollView>
-    )
-}
-
-const mapStateToProps = state => {
-    return {
-        selectedTab: state.tabReducer.selectedTab
+  const toast = useToast()
+  const showToast = (message, type) => {
+    toast.show(message, {
+      type: type,
+      placement: 'bottom',
+      duration: 2000,
+      swipeEnabled: true,
+      animationType: 'slide-in',
+      animationDuration: 100,
+    })
+  }
+  const readAuthData = async () => {
+    let value = null
+    try {
+      value = await AsyncStorage.getItem('currentUser')
+    } catch (e) {
+    } finally {
+      return JSON.parse(value)
     }
+  }
+
+  useEffect(() => {
+    readAuthData().then((response) => {
+      if (response?.token) {
+        axios.defaults.headers.common['Authorization'] = response.token
+        setAuthenticated(true)
+      }
+    })
+  }, [isAuthenticated])
+
+  const logout = async () => {
+    try {
+      await AsyncStorage.removeItem('currentUser')
+      axios.defaults.headers.common['Authorization'] = null
+      setAuthenticated(false)
+      showToast('Succesfully logged out', 'success')
+      navigation.navigate('LoginScreen')
+    } catch (e) {}
+  }
+
+  const handleLogin = () => {
+    navigation.navigate('LoginScreen')
+  }
+  return (
+    <DrawerContentScrollView
+      scrollEnabled={true}
+      contentContainerStyle={{ flex: 1 }}
+    >
+      <View style={{ flex: 1, paddingHorizontal: SIZES.radius }}>
+        <CloseMenu navigation={navigation} />
+        {/*Drawer Items*/}
+        {isAuthenticated && (
+          <View style={{ flex: 1, marginTop: SIZES.padding }}>
+            {DRAWER_ITEMS.slice(0, 7).map(({ id, label, icon, screen }) => (
+              <CustomDrawerItem
+                key={id}
+                label={label}
+                icon={icon}
+                isFocused={selectedTab === label}
+                onPress={() => {
+                  setSelectedTab(label)
+                  navigation.navigate(screen)
+                }}
+              />
+            ))}
+
+            <View
+              style={{
+                height: 1,
+                marginVertical: 40,
+                marginLeft: SIZES.radius,
+                backgroundColor: COLORS.darkBlue,
+              }}
+            />
+
+            {DRAWER_ITEMS.slice(7, 10).map(
+              ({ id, label, icon, iconType, screen }) => (
+                <CustomDrawerItem
+                  key={id}
+                  label={label}
+                  icon={icon}
+                  iconType={iconType}
+                  isFocused={selectedTab === label}
+                  onPress={() => {
+                    setSelectedTab(label)
+                    screen === 'AppInfoScreen'
+                      ? Alert.alert(
+                          'App Info',
+                          `Name: ${name}\nVersion: ${version}`,
+                          [{ text: 'OK' }],
+                        )
+                      : navigation.navigate(screen)
+                  }}
+                />
+              ),
+            )}
+          </View>
+        )}
+
+        {/*Login Button*/}
+        <View style={{ marginBottom: SIZES.padding }}>
+          {isAuthenticated ? (
+            <CustomDrawerItem
+              label={'Logout'}
+              icon={'logout'}
+              isFocused={false}
+              onPress={() => logout()}
+            />
+          ) : (
+            <>
+              <TherapymateLogo />
+              <CustomDrawerItem
+                label={'Login'}
+                icon={'login'}
+                isFocused={selectedTab === 'Login'}
+                onPress={() => handleLogin()}
+              />
+            </>
+          )}
+        </View>
+      </View>
+    </DrawerContentScrollView>
+  )
 }
 
-export default connect(mapStateToProps)(CustomDrawerContent)
+const mapStateToProps = (state) => {
+  return {
+    selectedTab: state.tabReducer.selectedTab,
+    isAuthenticated: state.authReducer.isAuthenticated,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  setSelectedTab: (label) => dispatch(setSelectedTab(label)),
+  setAuthenticated: (val) => dispatch(authenticate(val)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CustomDrawerContent)
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,32 +1,85 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   View,
-  Image,
   Text,
   StyleSheet,
-  TextInput,
   TouchableOpacity,
   SafeAreaView,
-  Switch,
+  Appearance,
+  useColorScheme,
 } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
-import {
-  AntDesign,
-  MaterialCommunityIcons,
-  MaterialIcons,
-  Ionicons,
-} from '@expo/vector-icons'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import Animated from 'react-native-reanimated'
 import { COLORS } from '../constants/theme'
+import { BASE_URL_APP } from '@env'
+import axios from 'axios'
+import Loader from '../components/Loader'
+import { TextInput, Switch } from 'react-native-paper'
+import { useToast } from 'react-native-toast-notifications'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import TherapymateLogo from '../components/TherapymateLogo'
+import { connect } from 'react-redux'
+import { authenticate } from '../stores/auth/authActions'
 
 const LoginScreen = (props) => {
-  const { drawerAnimationStyle } = props
+  const {
+    navigation,
+    drawerAnimationStyle,
+    isAuthenticated,
+    setAuthenticated,
+  } = props
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [rememberMe, setRememberMe] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+  const isDarkMode = useColorScheme() == 'dark'
+  const toast = useToast()
+  const showToast = (message, type) => {
+    toast.show(message, {
+      type: type,
+      placement: 'bottom',
+      duration: 2000,
+      swipeEnabled: true,
+      animationType: 'slide-in',
+      animationDuration: 100,
+    })
+  }
+
+  const saveAuthData = async (key, value) => {
+    try {
+      setAuthenticated(true)
+      await AsyncStorage.setItem(key, JSON.stringify(value))
+    } catch (e) {
+      showToast('Something went wrong', 'danger')
+    }
+  }
+
+  useEffect(() => {
+    setIsLoading(false)
+    isAuthenticated ? navigation.navigate('MainLayout') : null
+  }, [isAuthenticated])
 
   const [isEnabled, setIsEnabled] = useState(true)
   const toggleSwitch = () => setIsEnabled((previousState) => !previousState)
+
+  const login = () => {
+    setIsLoading(true)
+    const url = `${BASE_URL_APP}/sessions.json`
+    const params = { username: email, password: password }
+    axios
+      .post(url, {}, { params })
+      .then((response) => {
+        saveAuthData('currentUser', response['data'])
+        showToast(`Welcome ${response['data']['email']}`, 'success')
+        setIsLoading(false)
+        navigation.navigate('MainLayout')
+      })
+      .catch((err) => {
+        showToast('Invalid credentials. Please try again', 'danger')
+        setIsLoading(false)
+      })
+  }
 
   return (
     <Animated.View
@@ -47,57 +100,55 @@ const LoginScreen = (props) => {
             flexGrow: 1,
           }}
         >
-          <Image
-            style={styles.logo}
-            source={require('../assets/logo-tm.png')}
-          />
-
+          <Loader isLoading={isLoading} />
+          <TherapymateLogo />
           <View style={styles.inputView}>
             <TextInput
+              mode="outlined"
+              label="Email"
+              keyboardType="email-address"
               style={styles.TextInput}
-              placeholder="Email"
-              placeholderTextColor="#003f5c"
-              onChangeText={(email) => setEmail(email)}
-            />
-            <MaterialIcons
-              style={styles.textBoxIcon}
-              name="email"
-              size={20}
-              color="black"
+              activeOutlineColor={COLORS.blue}
+              outlineColor={COLORS.blue1}
+              placeholder="Enter Your Email"
+              placeholderTextColor={COLORS.blue1}
+              onChangeText={setEmail}
+              right={
+                <TextInput.Icon
+                  onPress={console.log()}
+                  name={'email-outline'}
+                  size={30}
+                  color={COLORS.blue1}
+                />
+              }
             />
           </View>
 
           <View style={styles.inputView}>
             <TextInput
+              mode="outlined"
+              label="Password"
               style={styles.TextInput}
-              placeholder="Password"
-              placeholderTextColor="#003f5c"
-              secureTextEntry={true}
-              onChangeText={(password) => setPassword(password)}
-            />
-            <MaterialIcons
-              style={styles.textBoxIcon}
-              name="lock"
-              size={20}
-              color="black"
+              activeOutlineColor={COLORS.blue}
+              outlineColor={COLORS.blue1}
+              placeholder="Enter Your Password"
+              placeholderTextColor={COLORS.blue1}
+              onChangeText={setPassword}
+              secureTextEntry
+              right={
+                <TextInput.Icon
+                  onPress={console.log()}
+                  name={'lock-outline'}
+                  size={30}
+                  color={COLORS.blue1}
+                />
+              }
             />
           </View>
 
           <View style={{ ...styles.loginAsBtnWrap, justifyContent: 'center' }}>
-            <Switch
-              trackColor={{ false: '#767577', true: '#88CBF1' }}
-              thumbColor={isEnabled ? '#20679F' : '#fff'}
-              ios_backgroundColor="#3e3e3e"
-              onValueChange={toggleSwitch}
-              value={isEnabled}
-              style={{ paddingRight: 0, marginTop: 1 }}
-            />
-            <Text
-              onPress={toggleSwitch}
-              style={{ fontWeight: 'bold', paddingTop: 8, marginLeft: 3 }}
-            >
-              Remember Me
-            </Text>
+            <Switch onValueChange={toggleSwitch} value={isEnabled} />
+            <Text style={styles.rememberMeText}>Remember Me</Text>
 
             <TouchableOpacity style={{ marginLeft: 35 }}>
               <Text style={styles.forgot_button}>Forgot Password?</Text>
@@ -105,10 +156,14 @@ const LoginScreen = (props) => {
           </View>
 
           <TouchableOpacity
-            onPress={() => props.navigation.navigate('MainLayout')}
+            onPress={login}
             style={{ ...styles.loginBtn, marginTop: 0 }}
           >
-            <AntDesign name="login" size={20} color="black" />
+            <MaterialCommunityIcons
+              name="login"
+              size={25}
+              color={COLORS.blue1}
+            />
             <Text style={styles.loginText}>LOGIN</Text>
           </TouchableOpacity>
         </ScrollView>
@@ -124,23 +179,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  logo: {
-    marginTop: 20,
-    marginBottom: 10,
-    height: 75,
-    width: 200,
-  },
   inputView: {
-    borderRadius: 10,
-    width: '85%',
-    marginBottom: 10,
-    borderWidth: 1,
-    borderColor: '#000',
+    width: '90%',
+    marginBottom: 20,
     margin: 2,
-    paddingLeft: 10,
-
-    flexDirection: 'row',
-    justifyContent: 'center',
   },
 
   textBoxIcon: {
@@ -151,6 +193,7 @@ const styles = StyleSheet.create({
   TextInput: {
     height: 50,
     flex: 1,
+    backgroundColor: COLORS.secondary,
   },
 
   forgot_button: {
@@ -160,24 +203,32 @@ const styles = StyleSheet.create({
     color: 'blue',
   },
 
+  rememberMeText: {
+    color: COLORS.blue1,
+    fontWeight: '600',
+    paddingTop: 8,
+    marginLeft: 3,
+  },
+
   loginBtn: {
-    width: '90%',
-    borderRadius: 20,
+    width: '100%',
+    borderRadius: 10,
     height: 50,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 10,
-    backgroundColor: '#88CBF1',
+    backgroundColor: COLORS.blue2,
     shadowColor: 'rgba(0, 0, 0, 0.1)',
-    shadowOpacity: 0.8,
-    elevation: 6,
-    shadowRadius: 15,
-    shadowOffset: { width: 1, height: 13 },
+    shadowOpacity: 0.7,
+    shadowRadius: 10,
+    shadowOffset: { width: 0, height: 5 },
   },
   loginText: {
     marginLeft: 5,
-    fontWeight: 'bold',
+    fontWeight: '500',
+    fontSize: 17,
+    color: COLORS.blue1,
   },
   loginAsText: {
     fontWeight: 'bold',
@@ -224,4 +275,15 @@ const styles = StyleSheet.create({
   },
 })
 
-export default LoginScreen
+const mapStateToProps = (state) => {
+  return {
+    selectedTab: state.tabReducer.selectedTab,
+    isAuthenticated: state.authReducer.isAuthenticated,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  setAuthenticated: (val) => dispatch(authenticate(val)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen)
