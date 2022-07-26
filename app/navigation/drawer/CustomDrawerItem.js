d9267c3b50945ddf47552584a292d7b03d52c225
Kamrul Hassan <kh27.abir@gmail.com>
2022-07-26 03:44:18 -0600

    refactor login button style

--- app/navigation/drawer/CustomDrawerItem.js
+++ app/navigation/drawer/CustomDrawerItem.js
@@ -1,31 +1,33 @@
-import {Image, Text, TouchableOpacity} from "react-native";
-import {COLORS, SIZES} from "../../constants/theme";
-import React from "react";
-import {AntDesign, MaterialCommunityIcons} from "@expo/vector-icons";
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { COLORS, SIZES } from '../../constants/theme'
+import React from 'react'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
-const CustomDrawerItem = ({label, icon, iconType, isFocused, onPress}) => {
-    return (
-        <TouchableOpacity
-            style={{
-                flexDirection: 'row',
-                height: 40,
-                marginBottom: SIZES.base,
-                alignItems: 'center',
-                paddingLeft: SIZES.radius,
-                borderRadius: SIZES.base,
-                backgroundColor: isFocused ? COLORS.transparentBlack1 : null
-            }}
-            onPress={onPress}
-        >
-            {label === 'Login' ?
-                <AntDesign name="login" size={20} color="black"/> :
-                <MaterialCommunityIcons name={icon} size={25} color="black"/>
-            }
-            <Text style={{fontSize: 16, fontWeight: 'bold', marginLeft: 15, color: COLORS.black}}>
-                {label}
-            </Text>
-        </TouchableOpacity>
-    )
+const CustomDrawerItem = ({ label, icon, iconType, isFocused, onPress }) => {
+  const backgroundColor = isFocused ? COLORS.transparentBlack1 : null
+  return (
+    <TouchableOpacity style={[container, backgroundColor]} onPress={onPress}>
+      <MaterialCommunityIcons name={icon} size={25} color="black" />
+      <Text style={itemLabel}>{label}</Text>
+    </TouchableOpacity>
+  )
 }
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    height: 40,
+    marginBottom: SIZES.base,
+    alignItems: 'center',
+    paddingLeft: SIZES.radius,
+    borderRadius: SIZES.base,
+  },
+  itemLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginLeft: 15,
+    color: COLORS.black,
+  },
+})
+const { container, itemLabel } = styles
 
 export default CustomDrawerItem
